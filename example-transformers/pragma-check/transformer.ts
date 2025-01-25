a1a6987c0e340854008212ba485b445a9bdadbb9
vuong <vuongdo_777@yahoo.com>
2025-01-25 08:01:34 +0000

    Refactor dependencies - refactoring code

--- example-transformers/pragma-check/transformer.ts
+++ example-transformers/pragma-check/transformer.ts
@@ -1,8 +1,8 @@
 import * as ts from 'typescript';
 
-let transformer: ts.TransformerFactory<ts.SourceFile> = context => {
+const transformer: ts.TransformerFactory<ts.SourceFile> = context => {
   return sourceFile => {
-    let jsxPragma = (sourceFile as any).pragmas.get('jsx');
+    const jsxPragma = (sourceFile as any).pragmas.get('jsx');
     if (jsxPragma) {
       console.log(`a jsx pragma was found using the factory "${jsxPragma.arguments.factory}"`);
     }
