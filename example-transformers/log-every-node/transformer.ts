6c019d6f0a027a0feb0f6d017c122281d883fafb
vuong <vuongdo_777@yahoo.com>
2025-01-29 04:00:24 +0000

    Document configuration - adding new feature

--- example-transformers/log-every-node/transformer.ts
+++ example-transformers/log-every-node/transformer.ts
@@ -1,8 +1,8 @@
 import * as ts from 'typescript';
 
-const transformer: ts.TransformerFactory<ts.SourceFile> = context => {
+let transformer: ts.TransformerFactory<ts.SourceFile> = context => {
   return sourceFile => {
-    const visitor = (node: ts.Node): ts.Node => {
+    let visitor = (node: ts.Node): ts.Node => {
       console.log(node.kind, `\t# ts.SyntaxKind.${ts.SyntaxKind[node.kind]}`);
       return ts.visitEachChild(node, visitor, context);
     };
